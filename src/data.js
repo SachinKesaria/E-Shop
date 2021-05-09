e8373f3a6f18a0e9809528ff4145fa45f6931ce2
Sachin <kesariasachin27@gmail.com>
2021-05-09 21:47:17 +0530

    Pushing Files

---
+++ src/data.js
@@ -0,0 +1,86 @@
+var laptops = [
+    {
+        "name": "Lenovo Ideapad Slim 3 AMD Athlon Silver 3050U 15.6 inch HD Thin and Light Laptop (4GB/1TB HDD/Windows 10/MS Office 2019/Platinum Grey/1.85Kg), 81W100HKIN", "img_src": "../images/l1.jpg", "price": "50000",
+        "desc": ["Processor: AMD Athlon Silver 3050U | Speed: 2.3 GHz (Base) - 3.2 GHz (Max) | 2 Cores | 4MB Cache", "OS: Pre-Loaded Windows 10 Home with Lifetime Validity", "Memory and Storage: 4GB RAM DDR4, Upgradable up to 12GB | 1TB HDD", "Display: 15.6 inches HD (1366x768) | Anti-Glare", "Ports: 2 USB 3.2 Gen1, 1 USB 2.0, Headphone/Mic combo jack, HDMI 1.4b, 4-in-1 media reader (MMC, SD, SDHC, SDXC)"]
+    },
+    {
+        "name": "Apple MacBook Air (13-inch, 1.1GHz Dual-core 10th-Generation Intel Core i3 Processor, 8GB RAM, 256GB Storage) - Silver", "img_src": "../images/l2.jpg", "price": "100000",
+        "desc": ["Tenth-generation Intel Core i3 processor", "Intel Iris Plus Graphics", "Up to 11 hours of battery life", "13.3-inch Retina display with True Tone technology", "Two Thunderbolt 3 (USB-C) ports"]
+    },
+    {
+        "name": "Lenovo Legion 5 AMD Ryzen 5 4600H 15.6 inch Full HD Gaming Laptop (8GB/1TB HDD + 256GB SSD/Windows 10/120 Hz/NVIDIA GTX 1650 4GB GDDR6 Graphics/Phantom Black/2.3Kg), 82B500BHIN", "img_src": "../images/l3.jpg", "price": "70000",
+        "desc": ["Processor: 4th Gen AMD Ryzen 5 (4600H) | Speed: 3.0 GHz (Base) - 4.0 GHz (Max) | 6 Cores | 8MB Cache", "OS: Pre-Loaded Windows 10 Home with Lifetime Validity", "Memory and Storage: 8GB RAM DDR4-3200, Upgradable up to 16GB | 1TB HDD + 256GB SSD", "13.3-inch Retina display with True Tone technology", "Graphics: NVIDIA GeForce GTX 1650 4GB GDDR6 Dedicated Graphics"]
+    },
+    {
+        "name": "HP Chromebook x360 Intel Celeron N4020 Processor 12-inch Touchscreen Laptop (Celeron N4020/4GB/64GB SSD/Chrome OS/Integrated Graphics/Natural Silver), 12b-ca0010TU", "img_src": "../images/l4.jpg", "price": "30000",
+        "desc": ["Graphics: Intel UHD Graphics 600", "Operating System: Chrome OS", "Memory & Storage: 4GB DDR4 RAM | 64 GB eMMC storage", "Camera & Microphone: HP TrueVision HD camera with integrated dual array digital microphone", "RAM Size	4 GB Memory Storage Capacity 64 GB"]
+    },
+    {
+        "name": "HP Pavilion x360 Core i3 Touchscreen 14-inch FHD Laptop (i3-10110U/8GB/256GB SSD/Win 10/MS Office/Mineral Silver/1.6kg)", "img_src": "../images/l5.jpg", "price": "45000",
+        "desc": ["Processor: 10th Gen Intel Core i3-10110U (2.1 GHz base frequency, up to 4.1 GHz with Intel Turbo Boost Technology, 4 MB L3 cache, 2 cores)", "Operating System & Preinstalled Software: Windows 10 Home | Microsoft Office Home & Student 2019 | In the box: Laptop with included battery, charger", "Display: 14-Inch FHD (1920 x 1080), touch-enabled, IPS, edge-to-edge glass, micro-edge, 250 nits, 45% NTSC | Without Inking Pen", "Graphics: Intel UHD Integrated Graphics", "Design & Battery: Touchscreen and Convertible Laptop | Laptop weight: 1.58 kg | Average battery life = 7 hours, 3-cell, 41 Wh Li-ion Fast Charge Battery"]
+    },
+    {
+        "name": "Dell Inspiron 5406 11th Gen 14inch FHD 2in1 Laptop(i5-1135G7 / 8GB / 512 SSD / Iris Xe Graphics /Win 10/ MS Office 19/ Active Pen/Titan Grey)", "img_src": "../images/l6.jpg", "price": "70000",
+        "desc": ["Processor:11th Generation Intel Core i5-1135G7 Processor (8MB Cache, up to 4.2 GHz)", "Memory & Storage:8GB RAM 3200MHz | 512 GB M.2 PCIe NVMe Solid State Drive", "Graphics:Intel Iris Xe Graphics with shared graphics memory", "Operating System & Software:Windows 10 Home Single Language | Microsoft Office Home and Student 2019 | McAfee Security Center 15 month subscription", "I/O ports:HDMI 2.0 Port | Two USB 3.2 Gen 1 ports |One Thunderbolt 4 port (Type-C) with Power Delivery | 1xHeadphone & Microphone Audio Jack"]
+    }
+];
+
+var smphones = [
+    {
+        "name":"Samsung Galaxy S20 FE 5G (Cloud Navy, 8GB RAM, 128GB Storage)","img_src":"../images/dm1.jpg","price":"50000",
+        "desc": ["5G Ready powered by Qualcomm Snapdragon 865 Octa-Core processor, 8GB RAM, 128GB internal memory expandable up to 1TB, Android 11.0 operating system and dual SIM","4500 mAh battery (Non -removable) with Super Fast Charging, FAst Wireless Charging & Finger Print sensor","6.5-inch(16.40 centimeters) Infinity-O Super AMOLED Display with 120Hz Refresh rate, 1080 x 2400 (FHD+) Resolution","Triple Rear Camera Setup - 12MP (Dual Pixel) OIS F1.8 Wide Rear Camera + 8MP OIS Tele Camera + 12MP Ultra Wide | 30X Space Zoom, Single Take & Night Mode | 32MP F2.2 Front Punch Hole Camera","Triple Rear Camera Setup - 12megapixels (Dual Pixel)"]
+    },
+    {
+        "name":"OnePlus Nord 5G (Blue Marble, 12GB RAM, 256GB Storage)","img_src":"../images/dm2.jfif","price":"30000",
+        "desc": ["5G Ready powered by Qualcomm Snapdragon 865 Octa-Core processor, 8GB RAM, 128GB internal memory expandable up to 1TB, Android 11.0 operating system and dual SIM","4500 mAh battery (Non -removable) with Super Fast Charging, FAst Wireless Charging & Finger Print sensor","6.5-inch(16.40 centimeters) Infinity-O Super AMOLED Display with 120Hz Refresh rate, 1080 x 2400 (FHD+) Resolution","Triple Rear Camera Setup - 12MP (Dual Pixel) OIS F1.8 Wide Rear Camera + 8MP OIS Tele Camera + 12MP Ultra Wide | 30X Space Zoom, Single Take & Night Mode | 32MP F2.2 Front Punch Hole Camera","Triple Rear Camera Setup - 12megapixels (Dual Pixel)"]
+    },
+    {
+        "name":" New Apple iPhone 12 Mini","img_src":"../images/m3.jpg","price":"70000",
+        "desc": ["5.4-inch (13.7 cm diagonal) Super Retina XDR display","Ceramic Shield, tougher than any smartphone glass","A14 Bionic chip, the fastest chip ever in a smartphone","Advanced dual-camera system with 12MP Ultra Wide and Wide cameras; Night mode, Deep Fusion, Smart HDR 3, 4K Dolby Vision HDR recording","12MP TrueDepth front camera with Night mode, 4K Dolby Vision HDR recording"]
+    },
+    {
+        "name":"Vivo X60 Pro (Midnight Black, 12GB RAM, 256GB Storage)","img_src":"../images/m4.jpg","price":"50000",
+        "desc": ["48MP+13MP+13MP Rear Camera | 32MP Selfie Camera","16.65cm (6.56 inches) AMOLED Display with 120Hz Refresh Rate and 2376 x 1080 pixels resolution.","Memory & SIM: 12GB RAM | 256GB internal memory | Dual SIM (nano+nano) dual-standby (5G).","Funtouch OS 11.1 (based on Android 11) operating system with Qualcomm Snapdragon 870 octa core processor","33W flash charging with 4200mAh battery (Type-C).","RAM	12 GB Product Dimensions: 15.9 x 7.3 x 0.8 cm; 177 Grams"]
+    },
+    {
+        "name":"Apple iPhone 11 Pro Max (64GB) - Space Grey","img_src":"../images/m5.jpg","price":"90000",
+        "desc": ["6.5-inch (16.5 cm diagonal) Super Retina XDR OLED display","Water and dust resistant (4 meters for up to 30 minutes, IP68)","Triple-camera system with 12MP Ultra Wide, Wide, and Telephoto cameras; Night mode, Portrait mode, and 4K video up to 60fps","12MP TrueDepth front camera with Portrait Mode, 4K video, and Slo-Mo","A13 Bionic chip with third-generation Neural Engine","Fast charge with 18W adapter included"]
+    },
+    {
+        "name":"Samsung Galaxy M51 (Electric Blue, 6GB RAM, 128GB Storage)","img_src":"../images/dm6.jpg","price":"23000",
+        "desc": ["Quad camera setup - 64MP (F1.8) main camera + 12MP (F2.2) ultra wide camera + 5MP (F2.4) depth camera + 5MP (F2.4) macro camera and 32MP (F2.2) front camera","16.95 cm (6.7-inch) sAMOLED Plus - Infinity-O display, FHD+ capacitive touchscreen with 1080 x 2400 pixels resolution, Contrast Ratio: 78960:1","Memory, Storage & SIM: 6GB RAM, 128GB internal memory expandable up to 512GB | Dedicated Memory slot","Android v10.0 operating system with 2.2GHz+1.8GHz Qualcomm | SD730G octa core processor","7000mAH lithium-ion battery | 25W USB Type-C to C fast charger in the box"]
+    }
+];
+
+var smwatches = [
+    {
+        "name":"Apple Watch Series 3 (GPS, 42mm) - Space Grey Aluminium Case with Black Sport Band","img_src":"../images/s1.jpg","price":"24000",
+        "desc": ["Retina display","Optical heart sensor","Store and stream music, podcasts and audiobooks","Emergency SOS","watchOS 6 with Activity trends, cycle tracking, loud noise notifications and the App Store on your wrist"]
+    },
+    {
+        "name":"Samsung Galaxy Watch Active 2 (Bluetooth, 44 mm) - Gold, Steel Dial, Leather Straps","img_src":"../images/ds2.jpg","price":"25000",
+        "desc": ["24/7 Activity Tracking - 4 stage Sleep, Activity with continuous HRM and Stress monitoring","39 built in trackers with 50m Water Resistance","Super AMOLED display with customizable Always on Watch faces","Product Dimensions	1.1 x 4.4 x 4.4 cm; 360 Grams"]
+    },
+    {
+        "name":"New Apple Watch Series 6 (GPS + Cellular, 44mm) - Blue Aluminium Case with Deep Navy Sport Band","img_src":"../images/s3.jpg","price":"53000",
+        "desc": ["The Always-On Retina display is 2.5x brighter outdoors when your wrist is down","Measure your blood oxygen with an all-new sensor and app","Check your heart rhythm with the ECG app","5GHz Wi-Fi and U1 Ultra Wideband chip"]
+    },
+    {
+        "name":"New Apple Watch SE (GPS, 44mm) - Silver Aluminium Case with White Sport Band","img_src":"../images/s4.jpg","price":"33000",
+        "desc": ["GPS model lets you take calls and reply to texts from your wrist","Track your daily activity on Apple Watch and see your trends in the Fitness app on iPhone","Measure workouts like running, walking, cycling, yoga, swimming, and dance","High and low heart rate notifications and irregular heart rhythm notification"]
+    }
+];
+
+var cart=[];
+var items=[];
+cart=getItems();
+function getItems(){
+    if(JSON.parse(sessionStorage.getItem("items"))!=null){
+        return JSON.parse(sessionStorage.getItem("items"));
+    }else{
+        return [];
+    }
+}
+function save(){
+    window.sessionStorage.setItem("items", JSON.stringify(cart));
+    }
